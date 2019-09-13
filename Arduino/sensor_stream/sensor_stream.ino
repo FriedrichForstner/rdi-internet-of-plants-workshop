4ea62bc34a85d3f04e04d19e74d5564eada3692a
friedrichfoerstner <friedrich@fold.ai>
2019-09-13 14:34:42 +0200

    New sensors added, adjusted .ino and .py code, detailed setup instructions.

--- Arduino/sensor_stream/sensor_stream.ino
+++ Arduino/sensor_stream/sensor_stream.ino
@@ -9,30 +9,79 @@
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
+#include "DHT.h"
+#include "Adafruit_TSL2591.h"
+#include "Adafruit_seesaw.h"
+#include <Adafruit_NeoPixel.h>
 
-Adafruit_BME680 bme;
+#define DHTTYPE DHT22
+#define DHTPIN 6
+#define LED_PIN 8
+#define LED_COUNT 1
+
+// init sensor objects
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_TSL2591 luminanceSensor = Adafruit_TSL2591(2591);
+Adafruit_seesaw soilSensor;
+
+// init LED
+Adafruit_NeoPixel pixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  !bme.begin();
+
+  // init environmental sensor
+  dht.begin();
+
+  // init soil sensor
+  soilSensor.begin(0x36);
+
+  // init luminance sensor
+  luminanceSensor.begin();
+  luminanceSensor.setGain(TSL2591_GAIN_LOW);
+  luminanceSensor.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+
+  // Info LED
+  pixel.begin();
+  pixel.show();
+
 }
 
 void loop() {
-  if (! bme.performReading()) {
-    Serial.println("Failed to perform reading :(");
-    return;
-  }
+
+  // air sensor
+  float airTemperature = dht.readTemperature(); // air temperature in C
+  float airHumidity = dht.readHumidity(); // air humidity
+
+  // soil sensor
+  float soilTemperature = soilSensor.getTemp();
+  uint16_t soilHumidity= soilSensor.touchRead(0);
+
+  // luminance sensor
+  sensors_event_t event;
+  luminanceSensor.getEvent(&event);
+  float luminance = event.light;
+
   // single line serial output with all parameter
-  // air_temperature, air_humidity, soil_humidity, brightness
-  Serial.print(bme.temperature);
+  // air_temperature, air_humidity, soil_temperature, soil_humidity, luminance
+  Serial.print(airTemperature);
   Serial.print(",");
-  Serial.print(bme.humidity);
+  Serial.print(airHumidity);
   Serial.print(",");
-  Serial.print(0);
+  Serial.print(soilTemperature);
   Serial.print(",");
-  Serial.print(0);
+  Serial.print(soilHumidity);
   Serial.print(",");
+  Serial.print(luminance);
   Serial.println("");
-  delay(1000);
+
+  delay(600);
+
+  pixel.setPixelColor(0, 255, 0, 0);
+  pixel.show();
+  delay(400);
+  pixel.setPixelColor(0, 0, 0, 0);
+  pixel.show();
+
 }
